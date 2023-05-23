a12e2b5e1494fad92a2fca5a34c912da47fcf16c
Fajarnurulm566 <fajarnurulmus@gmail.com>
2023-05-23 21:52:31 +0700

    Fresh Project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
+
+const Login = () => {
+    return (
+        <>
+            <Navbar/>
+            <div>Login</div>
+        </>
+    )
+}
+
+export default Login
